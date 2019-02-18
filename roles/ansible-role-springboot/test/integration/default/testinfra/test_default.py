c58439f55e1801777852995c05f504f4e64b6ae1
Simon Massey <simon@simonmassey.org>
2019-02-18 21:40:09 +0000

    test the microservice is installed and running

--- roles/ansible-role-springboot/test/integration/default/testinfra/test_default.py
+++ roles/ansible-role-springboot/test/integration/default/testinfra/test_default.py
@@ -1,3 +1,8 @@
-def test_port_80_is_listening(host):
+def test_port_8082_is_listening(host):
   socket = host.socket("tcp://8082")
   assert(socket.is_listening)
+
+def test_microservices_demo_running_and_enabled(host):
+    microservice = host.service("microservices-demo")
+    assert microservice.is_running
+    assert microservice.is_enabled
